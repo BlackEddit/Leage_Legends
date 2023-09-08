301f18ce1ac3861e572eccb4fcbee1f8c95b1ac2
BlackEddit <rasan.edd.p@gmail.com>
2023-09-08 13:34:02 -0600

    Proyect to compare the actual core/meta

---
+++ order_files.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+
+# Crear las carpetas ADC y SUP si no existen
+if not os.path.exists('ADC'):
+    os.makedirs('ADC')
+
+if not os.path.exists('SUP'):
+    os.makedirs('SUP')
+
+# Lista de campeones ADC y SUP (añade más según tu conocimiento)
+adc_champs = ['Ashe', 'Jhin', 'Caitlyn']
+sup_champs = ['Thresh', 'Leona', 'Lulu']
+
+# Mover los archivos a sus respectivas carpetas
+for filename in os.listdir('.'):
+    if filename.endswith('.json'):
+        champ_name = filename.split('.')[0]
+        if champ_name in adc_champs:
+            shutil.move(filename, f'ADC/{filename}')
+        elif champ_name in sup_champs:
+            shutil.move(filename, f'SUP/{filename}')
